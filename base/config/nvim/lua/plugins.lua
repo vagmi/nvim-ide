4dd83d09a22045d24f04f9f592c0c9437c920039
Vagmi Mudumbai <vagmi.mudumbai@gmail.com>
2022-12-24 21:40:03 +0000

    add rust to the base image and configure tree sitter and lsp

--- base/config/nvim/lua/plugins.lua
+++ base/config/nvim/lua/plugins.lua
@@ -9,6 +9,7 @@ return require('packer').startup(function()
   use 'pangloss/vim-javascript'
   use 'leafgarland/typescript-vim'
   use 'peitalin/vim-jsx-typescript'
+  use 'mbbill/undotree'
   use 'folke/tokyonight.nvim'
   use 'fatih/vim-go'
   use 'nvim-treesitter/nvim-treesitter' 
@@ -30,5 +31,27 @@ return require('packer').startup(function()
   }
   use({ "kelly-lin/telescope-ag", requires = { { "nvim-telescope/telescope.nvim" } } })
   use 'tpope/vim-surround'
+
+  use {
+  'VonHeikemen/lsp-zero.nvim',
+  requires = {
+    -- LSP Support
+    {'neovim/nvim-lspconfig'},
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},
+    {'hrsh7th/cmp-buffer'},
+    {'hrsh7th/cmp-path'},
+    {'saadparwaiz1/cmp_luasnip'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/cmp-nvim-lua'},
+
+    -- Snippets
+    {'L3MON4D3/LuaSnip'},
+    {'rafamadriz/friendly-snippets'},
+  }
+}
 end)
 
