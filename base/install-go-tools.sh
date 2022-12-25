5374bde1354168c34be4bfc7df7361a98e750e49
Vagmi Mudumbai <vagmi.mudumbai@gmail.com>
2022-12-25 02:04:34 +0000

    install go tools

---
+++ base/install-go-tools.sh
@@ -0,0 +1,20 @@
+export PATH=$PATH:/usr/local/go/bin
+
+go install github.com/klauspost/asmfmt/cmd/asmfmt@latest
+go install github.com/go-delve/delve/cmd/dlv@latest
+go install github.com/kisielk/errcheck@latest
+go install github.com/davidrjenni/reftools/cmd/fillstruct@master
+go install github.com/rogpeppe/godef@latest
+go install golang.org/x/tools/cmd/goimports@master
+go install github.com/mgechev/revive@latest
+go install golang.org/x/tools/gopls@latest
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+go install honnef.co/go/tools/cmd/staticcheck@latest
+go install github.com/fatih/gomodifytags@latest
+go install golang.org/x/tools/cmd/gorename@master
+go install github.com/jstemmer/gotags@master
+go install golang.org/x/tools/cmd/guru@master
+go install github.com/josharian/impl@master
+go install honnef.co/go/tools/cmd/keyify@master
+go install github.com/fatih/motion@latest
+go install github.com/koron/iferr@master
