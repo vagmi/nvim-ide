fd0176335b02d09186af5da1ef1f640df754ffb6
Vagmi Mudumbai <vagmi.mudumbai@gmail.com>
2022-12-30 03:31:32 +0000

    enable multiarch builds

--- base/install-go-tools.sh
+++ base/install-go-tools.sh
@@ -1,3 +1,23 @@
+set -e
+
+export ARCH=`uname -i`
+case $ARCH in 
+    x86_64)
+        export TARGETARCH=amd64
+        ;;
+    aarch64)
+        export TARGETARCH=arm64
+        ;;
+    *)
+        export TARGETARCH=$ARCH
+        ;;
+
+esac
+
+env
+mkdir -p /opt/sources &&  cd /opt/sources && wget -q https://go.dev/dl/go1.19.4.linux-$TARGETARCH.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf /opt/sources/go1.19.4.linux-$TARGETARCH.tar.gz && rm -rf /root/sources
+
 export PATH=$PATH:/usr/local/go/bin
 
 go install github.com/klauspost/asmfmt/cmd/asmfmt@latest
@@ -18,3 +38,5 @@ go install github.com/josharian/impl@master
 go install honnef.co/go/tools/cmd/keyify@master
 go install github.com/fatih/motion@latest
 go install github.com/koron/iferr@master
+
+cd /root
